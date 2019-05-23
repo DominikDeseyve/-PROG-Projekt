8cdcc72d9093ae63d435b59f60b331f8d9109afb
Anna <anna.kroesen@outlook.com>
2019-05-23 14:14:33 +0200

    unittest anfang

---
+++ contant_manager/unittest.cpp
@@ -0,0 +1,10 @@
+#define _UNITTEST_
+#include <person.h>
+#include <controller.h>
+
+using namespace ContactManager;
+
+using namespace std;
+
+#define CATCH_CONFIG_FAST_COMPILE
+#include <catch.hpp>
