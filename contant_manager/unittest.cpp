82b2eb7e89ac53d0ab441dac69905d077570581f
Anna <anna.kroesen@outlook.com>
2019-05-29 10:19:54 +0200

    Unittest

--- contant_manager/unittest.cpp
+++ contant_manager/unittest.cpp
@@ -8,3 +8,24 @@ using namespace std;
 
 #define CATCH_CONFIG_FAST_COMPILE
 #include <catch.hpp>
+
+
+const int SAMPLES_PERSON_COUNT = {3};
+
+const struct SamplesPerson
+{
+
+};
+
+SamplesPersonn SAMPLES_PERSON[SAMPLES_PERSON_COUNT] =
+{
+  SamplesPerson();
+  SamplesPerson();
+  SamplesPerson();
+}
+
+
+TEST_CASE("Constructor")
+{
+
+}
