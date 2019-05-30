2de045372b618be85bc44532d313e2b5a144a356
Anna <anna.kroesen@outlook.com>
2019-05-30 19:15:13 +0200

    <>

--- CONTACT_MANAGER/unittest.cpp
+++ CONTACT_MANAGER/unittest.cpp
@@ -1,13 +1,13 @@
 #define _UNITTEST_
-#include "person.h"
-#include "controller.h"
+#include <person.h>
+#include <controller.h>
 
 using namespace ContactManager;
 
 using namespace std;
 
 #define CATCH_CONFIG_FAST_COMPILE
-#include "catch.hpp"
+#include <catch.hpp>
 
 TEST_CASE("Pass Tests")
 {
