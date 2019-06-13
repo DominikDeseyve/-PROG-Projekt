3e2cfa90da4da133647fb9a7e0fb892c6df48216
Anna <anna.kroesen@outlook.com>
2019-06-13 22:09:43 +0200

    Unittest ready

--- CONTACT_MANAGER/unittest.cpp
+++ CONTACT_MANAGER/unittest.cpp
@@ -11,62 +11,17 @@ using namespace std;
 
 #include "catch.hpp"
 
-// kurzer Test obs laeuft
-/* TEST_CASE("Fail test")
-{
-    CHECK(1 == 0);
-}
-*/
-TEST_CASE("Pass Tests")
-{
-    CHECK(1 == 1);
-}
-
-/* // weiss it ob das Sinn ergibt
-const int SAMPLES_PERSON_COUNT = {3};
-
-const struct SamplesPerson
-{
-  string firstname;
-  string lastname;
-  GenderType gender;
-  uint32_t age;
-  uint32_t postcode;
-  string place;
-  string street;
-  uint32_t housenumber;
-  uint32_t prefix;
-  uint32_t phonenumber;
-
-  SamplesPerson(const string firstname, const string lastname, const GenderType gender, const uint32_t age,
-    const uint32_t age, const uint32_t postcode, const string place, const string street,
-    const uint32_t housenumber, const uint32_t prefix, const uint32_t phonenumber)
-  {
-
-  }
-
-};
-
-SamplesPersonn SAMPLES_PERSON[SAMPLES_PERSON_COUNT] =
-{
-  SamplesPerson(Rachel, Kroesen, 1, 21, 88069, Tettnang, Vogelherdbogen, 67, 07542, 939700);
-  SamplesPerson(Clemens, Maier-Schmidt, 0, 18, 79098, Freiburg, Seestraße, 4, 07664, 3902521);
-  SamplesPerson(Kasimir, Kalbrecht, 2, 19, 88045, Friedrichshafen, Rosiskeller, 2, 07541, 56402);
-}
-*/
-
-// method in controller.cpp
+// methods in controller.cpp
 TEST_CASE("loadContacts")
 {
   //pointer auf neues Objekt controller
   Controller *controller = new Controller();
+  controller->loadContacts();
   vector<Person> person = controller->getPersons();
   CHECK(person.size() > 0);
-
 }
 
 
-// methods in person.cpp
 TEST_CASE("intToEnum")
 {
     SECTION("maennlich")
@@ -135,11 +90,10 @@ TEST_CASE("enumToInt")
       string tmp = Controller::enumToInt(gender);
       CHECK(tmp == "divers");
     }
-
 }
 
 
-// methods in controller.cpp
+
 TEST_CASE("checkString")
 {
     // correct input
@@ -156,7 +110,6 @@ TEST_CASE("checkString")
       bool validate = Controller::checkString(input);
       CHECK(validate == true);
     }
-
 
     // wrong input
     SECTION("number", "String with numbers")
@@ -238,6 +191,37 @@ TEST_CASE("checkInt")
     {
       string input = "82.41";
       bool validate = Controller::checkInt(input);
+      CHECK(validate == false);
+    }
+}
+
+// Funktionenüberladung von checkInt
+TEST_CASE("checkRange", "check if int between min and max")
+{
+    SECTION("middle")
+    {
+      string input = "5";
+      int min = 1;
+      int max = 10;
+      bool validate = Controller::checkInt(input, min, max);
+      CHECK(validate == true);
+    }
+
+    SECTION("under")
+    {
+      string input = "0";
+      int min = 1;
+      int max = 10;
+      bool validate = Controller::checkInt(input, min, max);
+      CHECK(validate == false);
+    }
+
+    SECTION("over")
+    {
+      string input = "11";
+      int min = 1;
+      int max = 10;
+      bool validate = Controller::checkInt(input, min, max);
       CHECK(validate == false);
     }
 }
